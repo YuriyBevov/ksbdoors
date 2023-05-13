469c7d0939bb941841ab5fbc1327f5b5fb4bc7e9
Yuriy Bevov <jury19y@mail.ru>
2023-05-13 12:28:42 +0300

    add ymaps

--- build/assets/bundle.js
+++ build/assets/bundle.js
@@ -234,6 +234,163 @@ if (sliders) {
 
 /***/ }),
 
+/***/ "./src/scripts/modules/yMaps.js":
+/*!**************************************!*\
+  !*** ./src/scripts/modules/yMaps.js ***!
+  \**************************************/
+/***/ (() => {
+
+var map = document.querySelector('#yMaps');
+
+if (map) {
+  var desktopCoords = [59.897027478609516, 30.38424618243406];
+  var laptopCoords = [59.897027478609516, 30.38424618243406];
+  var mobileCoords = [59.897722811109205, 30.403515171997043];
+  var currentCoords = null;
+  var activeView = null;
+  var myMap = null;
+  var mapInited = null;
+  window.addEventListener('load', function () {
+    setMapCoords();
+  });
+  window.addEventListener('resize', function () {
+    setMapCoords();
+  });
+
+  var setMapCenter = function setMapCenter(coords) {
+    if (!mapInited) {
+      mapInited = true;
+      ymaps.ready(init);
+    } else {
+      myMap.setCenter(coords, 14, {
+        checkZoomRange: true
+      });
+    }
+  };
+
+  var setMapCoords = function setMapCoords() {
+    if (window.innerWidth < 960) {
+      if (activeView !== 'mobileView') {
+        activeView = 'mobileView';
+        currentCoords = mobileCoords;
+        setMapCenter(mobileCoords);
+      }
+    } else if (window.innerWidth > 959 && window.innerWidth < 1140) {
+      if (activeView !== 'laptopView') {
+        activeView = 'laptopView';
+        currentCoords = laptopCoords;
+        setMapCenter(laptopCoords);
+      }
+    } else {
+      if (activeView !== 'desktopView') {
+        activeView = 'desktopView';
+        currentCoords = desktopCoords;
+        setMapCenter(desktopCoords);
+      }
+    }
+  };
+
+  function init() {
+    // Создание карты.
+    myMap = new ymaps.Map("yMaps", {
+      center: currentCoords,
+      zoom: 14,
+      controls: [],
+      behaviors: ['drag']
+    });
+    var MyIconContentLayout = ymaps.templateLayoutFactory.createClass('<div class="ymaps-icon-content-layout">$[properties.iconContent]</div>'); // Метка
+
+    var officePlacemark = new ymaps.Placemark([59.90297506420561, 30.39827949999997], {
+      iconContent: 'Офис продаж'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#imageWithContent',
+      // Своё изображение иконки метки.
+      iconImageHref: 'assets/img/icon-office-pin.svg',
+      // Размеры метки.
+      iconImageSize: [60, 60],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-30, -30],
+      iconContentOffset: [60, 15],
+      iconContentLayout: MyIconContentLayout
+    });
+    var manufacturePlacemark = new ymaps.Placemark([59.89577706424945, 30.400462499999925], {
+      iconContent: 'Производство и склад'
+    }, {
+      // Опции.
+      // Необходимо указать данный тип макета.
+      iconLayout: 'default#imageWithContent',
+      // Своё изображение иконки метки.
+      iconImageHref: 'assets/img/icon-manufacture-pin.svg',
+      // Размеры метки.
+      iconImageSize: [60, 60],
+      // Смещение левого верхнего угла иконки относительно
+      // её "ножки" (точки привязки).
+      iconImageOffset: [-30, -30],
+      iconContentOffset: [60, 15],
+      iconContentLayout: MyIconContentLayout
+    });
+    myMap.geoObjects.add(officePlacemark);
+    myMap.geoObjects.add(manufacturePlacemark); // ZOOM-CONTROL
+
+    var ZoomLayout = ymaps.templateLayoutFactory.createClass( //Шаблон html кнопок зума
+    "<div class='zoom-btns'>" + "<button id='zoom-in' class='zoom-btn zoom-btn-in' aria-label='Увеличить масштаб'>" + "<svg width='14' height='14'>" + "<use xlink:href='./assets/sprite.svg#icon-zoom-in'>" + "</svg>" + "</button>" + "<button id='zoom-out' class='zoom-btn zoom-btn-out' aria-label='Уменьшить масштаб'>" + "<svg width='14' height='2'>" + "<use xlink:href='./assets/sprite.svg#icon-zoom-out'>" + "</svg>" + "</button>" + "</div>", {
+      // Переопределяем методы макета, чтобы выполнять дополнительные действия
+      // при построении и очистке макета.
+      build: function build() {
+        // Вызываем родительский метод build.
+        ZoomLayout.superclass.build.call(this); // Привязываем функции-обработчики к контексту и сохраняем ссылки
+        // на них, чтобы потом отписаться от событий.
+
+        this.zoomInCallback = ymaps.util.bind(this.zoomIn, this);
+        this.zoomOutCallback = ymaps.util.bind(this.zoomOut, this); // Начинаем слушать клики на кнопках макета.
+
+        var zoomInBtn = document.getElementById('zoom-in');
+        var zoomOutBtn = document.getElementById('zoom-out');
+        zoomInBtn.addEventListener('click', this.zoomInCallback);
+        zoomOutBtn.addEventListener('click', this.zoomOutCallback);
+      },
+      clear: function clear() {
+        // Снимаем обработчики кликов.
+        zoomInBtn.removeEventListener('click', this.zoomInCallback);
+        zoomOutBtn.removeEventListener('click', this.zoomOutCallback); // Вызываем родительский метод clear.
+
+        ZoomLayout.superclass.clear.call(this);
+      },
+      zoomIn: function zoomIn() {
+        myMap.balloon.close();
+        var map = this.getData().control.getMap();
+        map.setZoom(map.getZoom() + 1, {
+          checkZoomRange: true
+        });
+      },
+      zoomOut: function zoomOut() {
+        myMap.balloon.close();
+        var map = this.getData().control.getMap();
+        map.setZoom(map.getZoom() - 1, {
+          checkZoomRange: true
+        });
+      }
+    });
+    var zoomControl = new ymaps.control.ZoomControl({
+      options: {
+        layout: ZoomLayout,
+        position: {
+          right: '30px',
+          bottom: '50px'
+        }
+      }
+    });
+    myMap.controls.add(zoomControl);
+  }
+
+  ;
+}
+
+/***/ }),
+
 /***/ "./node_modules/desandro-matches-selector/matches-selector.js":
 /*!********************************************************************!*\
   !*** ./node_modules/desandro-matches-selector/matches-selector.js ***!
@@ -16098,6 +16255,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_swiper_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/swiper.js */ "./src/scripts/modules/swiper.js");
 /* harmony import */ var _modules_fancybox_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/fancybox.js */ "./src/scripts/modules/fancybox.js");
 /* harmony import */ var _modules_masonry_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/masonry.js */ "./src/scripts/modules/masonry.js");
+/* harmony import */ var _modules_yMaps_js__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/yMaps.js */ "./src/scripts/modules/yMaps.js");
+/* harmony import */ var _modules_yMaps_js__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_modules_yMaps_js__WEBPACK_IMPORTED_MODULE_4__);
+
 
 
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,3 +2,5 @@ import "./modules/limitStr.js";
 import "./modules/swiper.js";
 import "./modules/fancybox.js";
 import "./modules/masonry.js";
+import "./modules/yMaps.js";
+
